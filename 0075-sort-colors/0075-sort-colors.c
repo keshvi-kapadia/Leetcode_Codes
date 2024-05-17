2880eff4b86ce9b8bdc999a3cf6d14a26a9ccd48
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-05-17 15:50:01 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (82.00%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,14 @@
+void sortColors(int* a, int n) {
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+}
