a1664849e3eab21a62e072c711f8077ca1288ad4
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-05-23 18:20:11 +0530

    Time: 36 ms (5.27%), Space: 8.2 MB (5.70%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,32 @@
+int compare(const void *a,const void *b) {
+    int *x = (int *) a;
+    int *y = (int *) b;
+    return *x - *y;
+}
+  int majorityElement(int* a, int n) {
+    int i,j;
+
+    qsort (a, n, sizeof(int), compare);
+
+    int cnt=0,half=n/2;
+    i=0,j=0;
+    while(i<n)
+    {
+        while(j<n)
+        {
+            if(a[j]==a[i])
+            {
+            cnt++;
+            j++;
+             if(cnt>half)
+                {
+                return a[i];
+                }
+            }
+            else break;
+        }
+        cnt=0;
+        i++;
+    }
+    return 0;
+}
