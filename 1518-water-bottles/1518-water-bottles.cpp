b50832299ead09050be941c18a03b0091e078bf4
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-07-08 18:23:44 +0530

    Time: 2 ms (54.02%), Space: 7.3 MB (30.83%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int b, int e) {
+       int empty=b,ans=b;
+    while(empty>=e)
+    {
+        empty-=e;
+        ans+=1;
+        empty+=1;
+    }
+    return ans; 
+    }
+};
