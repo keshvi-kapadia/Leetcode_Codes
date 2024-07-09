4fb9b60517cec1a906435c8f26f88446b7302719
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-07-09 09:08:04 +0530

    Time: 147 ms (66.18%), Space: 76.4 MB (83.82%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& a) {
+        int m=a.size();
+         unsigned long long int time=a[0][0];
+    unsigned long long int wait=0;
+    unsigned int i=0;
+    while(i<m)
+    {
+        if(a[i][0]>time)
+        {
+            wait+=a[i][1];
+            time=a[i][0]+a[i][1];
+        }
+        else
+        {
+            time+=a[i][1];
+            wait+=time-a[i][0];
+        }
+        i++;
+    }
+    return (double)wait/m;
+    }
+};
