d1d3bb255bae4b19a95c4609dd6866707cc8bb5e
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-07-17 16:32:32 +0530

    Time: 116 ms (80.34%), Space: 80.3 MB (13.80%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -3,9 +3,8 @@ public:
     int matchPlayersAndTrainers(vector<int>& a1, vector<int>& a2) {
         sort(a1.begin(), a1.end()); 
         sort(a2.begin(), a2.end()); 
-        int n1=a1.size(),n2=a2.size();
         int i=0,j=0,cnt=0;
-    while(i<n1 && j<n2)
+    while(i<a1.size() && j<a2.size())
     {
         if(a1[i]<=a2[j])
         {
