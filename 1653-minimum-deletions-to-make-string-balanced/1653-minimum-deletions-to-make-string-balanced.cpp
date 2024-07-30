9a00a3e686878fb1288c3a2c5fa6e12845130100
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-07-30 09:30:30 +0530

    Time: 110 ms (31.74%), Space: 53.3 MB (14.35%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumDeletions(string s)
+    {
+        int n=s.length();
+        vector<int> count_a(n, 0);
+        vector<int> count_b(n, 0);
+        int b_count = 0;
+        for(int i=0;i<n;i++)
+        {
+            count_b[i]=b_count;
+            if(s[i]=='b')
+            b_count++;
+        }
+        int a_count = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            count_a[i]=a_count;
+            if(s[i]=='a')
+            a_count++;
+        }  
+        int min_deletions = n;
+        for(int i=0;i<n;i++)
+        {
+            if(min_deletions>count_a[i]+count_b[i])
+            min_deletions=count_a[i]+count_b[i];
+        }
+        return min_deletions;
+    }
+};
