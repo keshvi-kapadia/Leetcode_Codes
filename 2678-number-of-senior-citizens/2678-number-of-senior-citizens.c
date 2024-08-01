abecbc4dea22dca979c935f9fd45585ddd56a337
Keshvi Kapadia <150351480+Keshvi123@users.noreply.github.com>
2024-08-01 19:25:42 +0530

    Time: 8 ms (27.00%), Space: 8.7 MB (34.00%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.c
@@ -0,0 +1,10 @@
+int countSeniors(char ** a, int n){
+    int cnt=0,age;
+    for(int i=0;i<n;i++)
+    {
+        age=(a[i][11]-'0')*10+(a[i][12]-'0');
+        if(age>60)
+        cnt++;
+    }
+    return cnt;
+}
